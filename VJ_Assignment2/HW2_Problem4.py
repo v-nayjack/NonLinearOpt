c20ead4fd281828068b01a68eee6c6469a2fa519
Vinay J <jakkalvy@ucmail.uc.edu>
2019-12-11 19:16:52 -0500

    4 completed, Plots remaining

--- VJ_Assignment2/HW2_Problem4.py
+++ VJ_Assignment2/HW2_Problem4.py
@@ -17,63 +17,66 @@ def f1(x):
 
     return F1
 
-def f2(x):
-
-    F2 = -1. * ((x[0]**2 + x[1] - 11)**2 + (x[0] + x[1]**2 - 7)**2)
-
-    return F2
-
 def main1():
 
-    L = np.array([-10.0, -10.0])
-    U = np.array([10.0, 10.0])
-    x = np.linspace(L[0], U[0], 11)
-    y = np.linspace(L[1], U[1], 11)
-    Minima = []
-    Maxima = []
+    Fmins = lambda x: f1(x)
+    Fmaxs = lambda x: -1 * f1(x)
 
-    for i in x:
-        for j in y:
-            Minimums, k1 = powell_method(f1, [i, j], L, U)
-            Maximums, k2 = powell_method(f2, [i, j], L, U)
-            print("Minimums", Minimums)
-            print("Maximums", Maximums)
-            Minima.append(Minimums)
-            Maxima.append(Maximums)
-    print("Minima", Minima)
-    print("Maxima", Maxima)
+    L = np.array([-5.0, -5.0])
+    U = np.array([5.0, 5.0])
+#    X0 = np.array([[-5., -5.], [5., -5.], [-3., 3.], [5., 5.], [3., 2.]])
+    X0 = np.array([[-5., -2.], [5., -2.5], [-3., 3.8], [4.75, 1.8], [3., 2.]])
+    Minimums = np.zeros(np.shape(X0))
+    Maximums = np.zeros(np.shape(X0))
+
+    F_mins = np.zeros(len(X0))
+    F_maxs = np.zeros(len(X0))
+
+    for i in range(len(X0)):
+        Minimums[i, :], k1 = powell_method(Fmins, X0[i, :], L, U)
+        F_mins[i] = Fmins(Minimums[i, :])
+        Maximums[i, :], k2 = powell_method(Fmaxs, X0[i, :], L, U)
+        F_maxs[i] = Fmaxs(Maximums[i, :])
+
+    return np.around(Minimums, decimals=2), np.around(Maximums, decimals=2), F_mins, F_maxs
 
 
 def main2():
 
-    L = np.array([-5.0, -5.0])
-    U = np.array([5.0, 5.0])
-    x = np.linspace(L[0], U[0], 11)
-    y = np.linspace(L[1], U[1], 11)
-    Minima = []
-    Maxima = []
+    Fmins = lambda x: f1(x)
+    Fmaxs = lambda x: -1 * f1(x)
 
-    for i in x:
-        for j in y:
-            Minimums, k1 = quasi_newton(f1, [i, j])
-            Maximums, k2 = quasi_newton(f2, [i, j])
-            print("Minimums", Minimums)
-            print("Maximums", Maximums)
-            Minima.append(Minimums)
-            Maxima.append(Maximums)
-    print("Minima", Minima)
-    print("Maxima", Maxima)
+    X0 = np.array([[-5., -2.], [5., -2.5], [-3., 3.8], [4.75, 1.8], [3., 2.]])
+    Minimums = np.zeros(np.shape(X0))
+    Maximums = np.zeros(np.shape(X0))
 
+    F_mins = np.zeros(len(X0))
+    F_maxs = np.zeros(len(X0))
 
+    for i in range(len(X0)):
+        Minimums[i, :], k1 = quasi_newton(Fmins, X0[i, :])
+        F_mins[i] = Fmins(Minimums[i, :])
+        Maximums[i, :], k2 = quasi_newton(Fmaxs, X0[i, :])
+        F_maxs[i] = Fmaxs(Maximums[i, :])
 
-
-
-
+    return np.around(Minimums, decimals=2), np.around(Maximums, decimals=2), F_mins, F_maxs
 
 
 
 if __name__ == '__main__':
-    main1()
+    Mins, Maxs, f_mins, f_maxs = main2()
+
+    TotalMins = np.unique(f_mins)
+    TotalMaxs = np.where(np.unique(f_maxs >= 0.))[0]
+
+
+#    print("Minimums", Mins)
+#    print("Maximums", Maxs)
+#    print("Fmins", f_mins)
+#    print("Fmaxs", f_maxs)
+    print("Total no. of minimums", len(TotalMins))
+    print("Total no. of maximums", len(TotalMaxs))
+
 
 
 
